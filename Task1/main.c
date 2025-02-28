50810230ef993408f96937a4f29fce5e160406ee
ColtLead <ffeytu@gmail.com>
2025-02-28 22:02:22 +1000

    Add files via upload

--- Task1/main.c
+++ Task1/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main()
+{
+	double a = 2, b = -3, c = 1;
+	double discriminant, x1, x2;
+	discriminant = pow(b,2) - 4 * a * c;
+	x1 = (-b + sqrt(discriminant)) / (2 * a);
+	x2 = (-b - sqrt(discriminant)) / (2 * a);
+	printf("x1 = %.2f, x2 = %.2f\n", x1, x2);
+	return 0;
+}
