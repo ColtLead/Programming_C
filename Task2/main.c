e129a5e222bca3dbcf44d1327d1615664450eb28
ColtLead <ffeytu@gmail.com>
2025-03-01 19:01:20 +1000

    Add files via upload

--- Task2/main.c
+++ Task2/main.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+int main() {
+	double matrix3[3][3];
+	double maindiagonal = 0.0, sidediagonal = 0.0;
+	printf("input elements 3x3:\n");
+	for (int i =0; i < 3; i++) {
+		for (int j = 0; j <3; j++) {
+			scanf("%lf", &matrix3[i][j]);
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		maindiagonal += matrix3[i][i];
+		sidediagonal += matrix3[i][2-i];
+	}
+	printf("main diagonal %.2lf\n", maindiagonal);
+	printf("side diagonal %.lf\n", sidediagonal);
+
+	int matrix2[2][2];
+	int result[2][2]={0};
+	printf("input elements 2x2:\n)");
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			scanf("%d", &matrix2[i][j]);
+		}
+	}
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				result[i][j] += matrix2[i][k] * matrix2[k][j];
+			}
+		}
+	}
+	printf("square of :\n");
+	for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
