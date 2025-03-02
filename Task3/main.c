0b3c7dbb1e64a4320d19bdead4fc9ac7b8f05949
ColtLead <ffeytu@gmail.com>
2025-03-02 23:36:38 +1000

    Add files via upload

--- Task3/main.c
+++ Task3/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+	char str[81];
+	printf("Input up to 80:");
+	fgets(str, 81, stdin);
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == 'a') {
+			str[i] = 'A';
+		}
+		else if (str[i] == 'b') {
+			str[i] = 'B';
+		}
+	}
+	printf("Result: %s", str);
+	return 0;
+}
